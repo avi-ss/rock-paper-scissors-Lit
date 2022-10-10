d2fa296d4e6ed6bc3aa6d534f8acf64088bf300f
Alberto Longo <alongog@minsait.com>
2022-10-10 16:29:53 +0200

    Added ranking table

--- rock-paper-scissors/src/view/ranking-view.js
+++ rock-paper-scissors/src/view/ranking-view.js
@@ -1,11 +1,13 @@
 import { LitElement, html, css } from "lit";
 
+import "@vaadin/grid";
+
 export class RankingView extends LitElement {
   static get properties() {
     return {
       users: {
         type: Array,
-      }
+      },
     };
   }
 
@@ -16,16 +18,37 @@ export class RankingView extends LitElement {
   }
 
   firstUpdated() {
+    for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i) != "user") {
+        this.users.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+      }
+    }
+
+    this.users = [...this.users];
   }
 
   render() {
     return html`
-    <h1>RANKING</h1>
+      <div class="container">
+        <vaadin-grid .items="${this.users}">
+          <vaadin-grid-column path="name"></vaadin-grid-column>
+          <vaadin-grid-column path="gender"></vaadin-grid-column>
+          <vaadin-grid-column path="wins"></vaadin-grid-column>
+          <vaadin-grid-column path="defeats"></vaadin-grid-column>
+        </vaadin-grid>
+      </div>
     `;
   }
 
   static get styles() {
     return css`
+      .container {
+        display: flex;
+        align-items: center;
+        margin: auto;
+        margin-top: 20px;
+        width: 60%;
+      }
     `;
   }
 }
